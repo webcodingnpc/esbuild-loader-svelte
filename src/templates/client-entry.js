c7ace705bb39ddf14481f53e54c0b1a0fe408464
shadowWin <2390198813@qq.com>
2026-04-08 23:18:24 +0800

    fix: bump to v1.3.0, fix hydrate template in dist, update devDeps (esbuild 0.28, sass 1.99, svelte 5.55.2)

--- src/templates/client-entry.js
+++ src/templates/client-entry.js
@@ -1,8 +1,8 @@
 import '{{GLOBAL_SCSS}}'
-import { mount } from 'svelte'
+import { hydrate } from 'svelte'
 import App from '{{VIEW_PATH}}'
 
-const app = mount(App, {
+const app = hydrate(App, {
     target: document.getElementById('app'),
 })
 
